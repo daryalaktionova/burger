83deb663e994666e9c91e4f63808900aa30128df
daryalaktionova <segezmundovna@gmail.com>
2018-08-15 23:10:15 +0300

    Add js accordeon team

--- app/js/module.js
+++ app/js/module.js
@@ -1,42 +1,72 @@
 /* Overlay menu */
-let menu = (function(options) {
-
-  let button = document.querySelector(options.button);
-  let menu = document.querySelector(options.menu);
-  let body = document.querySelector('body');
-  let link = document.querySelectorAll(".overlay-menu__link");
-  
-
-
-
-  let _toggleMenu = function(e) {
-    button.classList.toggle('hamburger-bar--active');
-    menu.classList.toggle('overlay--open');
-    body.classList.toggle('body-active-menu');
-  }
-
-  let addListeners = function() {
-    button.addEventListener('click', _toggleMenu);
-    for (key in link) {
-        link[key].addEventListener('click', _toggleMenu);
-      }
-  }
 
   
-  return {
-      openMenu: addListeners
-    };
+  let menu = (function(options) {
 
-})({
-  button: '#toggle',
-  menu: '#overlay'
-});
-
-menu.openMenu();
+    let button = document.querySelector(options.button);
+    let menu = document.querySelector(options.menu);
+    let body = document.querySelector('body');
+    let link = document.querySelectorAll(".overlay-menu__link");
+    
 
 
 
+    let _toggleMenu = function(e) {
+      button.classList.toggle('hamburger-bar--active');
+      menu.classList.toggle('overlay--open');
+      body.classList.toggle('body-active-menu');
+    }
 
+    let addListeners = function() {
+      button.addEventListener('click', _toggleMenu);
+        for (i=0;i<link.length;i++){
+          link[i].addEventListener('click', _toggleMenu);
+        }
+    }
+
+    
+    return {
+        openMenu: addListeners
+      };
+
+  })({
+    button: '#toggle',
+    menu: '#overlay'
+  });
+
+  menu.openMenu();
 
 
 /*end overlay menu */
+
+/*Accodeon team*/
+let teamAccordeon = (function(options) {
+
+    let linkAccordeon = document.querySelectorAll(options.link);
+
+    let _toggleAccordeon = function(e) {
+      e.preventDefault();
+      this.parentNode.classList.toggle('accordeon__item--is-active');
+    }
+
+    let addListenersAccordeon = function() {
+       for (i=0;i<linkAccordeon.length;i++){
+          linkAccordeon[i].addEventListener('click', _toggleAccordeon);
+        }
+    }
+
+
+
+    
+    return {
+        openItemAccordeon: addListenersAccordeon
+      };
+
+  })({
+    link: '.accordeon__link'
+  });
+
+  teamAccordeon.openItemAccordeon();
+
+
+/*end Accordeon team */
